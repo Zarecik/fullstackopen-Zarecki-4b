1ce5da4a3818ee951f5b0011307a1963b45e96bb
Zarecik <szymonzarecki43@gmail.com>
2025-05-22 11:51:33 +0200

    part2.8nieskoczone

--- part2/zadania/src/App.jsx
+++ part2/zadania/src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '888-888-888'  }
   ])
 
   const [newName, setNewName] = useState('')
 
-  const handleInputChange = (event) => {
+  const [newNumber, setNewNumber] = useState('')
+
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
 
@@ -22,7 +28,7 @@ const App = () => {
       return 
     }
 
-    const newPerson = { name: newName }
+    const newPerson = { name: newName, number: newNumber }
 
     setPersons(persons.concat(newPerson))
 
@@ -34,7 +40,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleInputChange} />
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
