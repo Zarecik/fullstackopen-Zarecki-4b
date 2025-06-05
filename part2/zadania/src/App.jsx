05e1fe5c2c493ec9269a9f1e94940350b77a8be2
Szymon <szymonzarecki43@gmail.com>
2025-06-05 20:32:43 +0200

    2.12

--- part2/zadania/src/App.jsx
+++ part2/zadania/src/App.jsx
@@ -1,70 +1,67 @@
-import { useState } from 'react'
-import Filter from './Filter'
-import PersonForm from './PersonForm'
-import Persons from './Persons'
+import React, { useState, useEffect } from 'react'
+import personService from './persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '888-888-888'},
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  useEffect(() => {
+    personService.getAll().then(initialPersons => {
+      setPersons(initialPersons)
+    })
+  }, [])
 
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value)
-  }
-
-  const handleSubmit = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
-    const nameExists = persons.some(person => person.name === newName)
-    if (nameExists) {
-      alert(`${newName} is already added to phonebook`)
-      return 
+
+    const personObject = {
+      name: newName,
+      number: newNumber,
     }
 
-    const newPerson = { name: newName, number: newNumber }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-    setNewNumber('')
-  }
+    // Sprawdź czy już istnieje
+    const alreadyExists = persons.some(person => person.name === newName)
+    if (alreadyExists) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
 
-  const filteredPersons = persons.filter(person => 
-    person.name.toLowerCase().includes(filter.toLowerCase()) )
+    // Wyślij do serwera
+    personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+      })
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter filter={filter} onFilterChange={handleFilterChange} />
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={e => setNewName(e.target.value)} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={e => setNewNumber(e.target.value)} />
+        </div>
+        <div><button type="submit">add</button></div>
+      </form>
 
-      <h2>Add new</h2>
-      <PersonForm
-        onSubmit={handleSubmit}
-        newName={newName}
-        handleNameChange={handleNameChange}
-        newNumber={newNumber}
-        handleNumberChange={handleNumberChange}
-      />
-
-      <h3>Numbers</h3>
-      <Persons persons={filteredPersons} />
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <li key={person.id}>{person.name} {person.number}</li>
+        )}
+      </ul>
     </div>
   )
 }
 
 export default App
+
 
 
 
