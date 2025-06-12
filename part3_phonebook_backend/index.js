d27c4ce77d3620a1f8731a0faf83730957f2e8db
Szymon <szymonzarecki43@gmail.com>
2025-06-13 00:43:05 +0200

    3.2

--- part3_phonebook_backend/index.js
+++ part3_phonebook_backend/index.js
@@ -1,38 +1,46 @@
-// 1. Importujemy Express
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-// 2. Dane na sztywno – książka telefoniczna
 const persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
+  { 
+    id: "1",
+    name: "Arto Hellas", 
     number: "040-123456"
   },
-  {
-    id: 2,
-    name: "Ada Lovelace",
+  { 
+    id: "2",
+    name: "Ada Lovelace", 
     number: "39-44-5323523"
   },
-  {
-    id: 3,
-    name: "Dan Abramov",
+  { 
+    id: "3",
+    name: "Dan Abramov", 
     number: "12-43-234345"
   },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
+  { 
+    id: "4",
+    name: "Mary Poppendieck", 
     number: "39-23-6423122"
   }
-];
+]
 
-// 3. Endpoint GET /api/persons – wysyła dane jako JSON
+// endpoint z listą osób z 3.1
 app.get('/api/persons', (req, res) => {
-  res.json(persons);
-});
+  res.json(persons)
+})
 
-// 4. Uruchamiamy serwer na porcie 3001
-const PORT = 3001;
+// nowy endpoint z zadania 3.2
+app.get('/info', (req, res) => {
+  const date = new Date()
+  const entriesCount = persons.length
+
+  res.send(`
+    <p>Phonebook has info for ${entriesCount} people</p>
+    <p>${date}</p>
+  `)
+})
+
+const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
